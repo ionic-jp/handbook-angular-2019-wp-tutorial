1ccf85f181e62b72d9b348854cf2fbb1d46d34cc
榊原昌彦 <sakakibara@rdlabo.jp>
2019-09-18 14:27:50 +0900

    STEP3　記事詳細ページを実装 STEP4　UXを高めるための非同期処理

---
+++ src/app/article/article.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.page.html',
+  styleUrls: ['./article.page.scss'],
+})
+export class ArticlePage implements OnInit {
+  ID: number;
+  post: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string;
+  } = {
+    ID: null,
+    title: null,
+    content: null,
+    date: null
+  };
+  constructor(public route: ActivatedRoute, public http: HttpClient) { }
+
+  ngOnInit() {
+    this.route.paramMap
+      .subscribe((params: ParamMap) => {
+        this.ID = parseInt(params.get('articleId'), 10);
+      });
+  }
+
+  ionViewDidEnter() {
+    this.http.get<{
+      ID: number;
+      title: string;
+      content: string;
+      date: string;
+    }>('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/'  + this.ID)
+  .subscribe(data => {
+      this.post = data;
+    });
+  }
+}
