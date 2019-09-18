af7926e87c95f0d250cf98b8888ff5735bfaeef7
榊原昌彦 <sakakibara@rdlabo.jp>
2019-09-18 14:11:49 +0900

    STEP2　記事一覧を取得して表示

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  posts: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string;
+  }[] = [];
+  constructor(
+    public http: HttpClient,
+    public loadingController: LoadingController,
+  ) {}
 
-  constructor() {}
-
+  async ionViewDidEnter() {
+    const loading = await this.loadingController.create({
+      message: 'Loading...',
+    });
+    await loading.present();
+    // this.http.get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/')
+    //   .subscribe(data => {
+    //     this.posts = data['posts'];
+    //     loading.dismiss();
+    //   });
+  }
 }
