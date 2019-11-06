de6c34bb8fd1fde49b46f1448f85be24fa818dd0
榊原昌彦 <sakakibara@rdlabo.jp>
2019-11-06 18:36:52 +0900

    ステップ⑤　Google Analyticsを設定してアクセス解析を行う

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,11 +10,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public router: Router,
   ) {
     this.initializeApp();
   }
@@ -23,5 +25,14 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  ngOnInit() {
+    this.router.events
+      .subscribe(params => {
+        if (params instanceof NavigationEnd && !this.platform.is('hybrid')) {
+          gtag('config', 'UA-*****-**', { page_path: params.url });
+        }
+      });
   }
 }
